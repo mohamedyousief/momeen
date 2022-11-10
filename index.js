8068cbe71624c64ba9f3deaa77f776f813786f59
mohamedyousief <mohamedyousiefo3@gmail.com>
2022-11-10 15:22:09 +0200

    done

---
+++ index.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('.navn a').forEach(anchor=>{
+    anchor.addEventListener("click",()=>{
+        const el=document.getElementById(anchor.getAttribute("data-link"))
+        el.scrollIntoView({behavior:"smooth",block:"start"})
+     })
+})
+function changePhoto(src){
+    document.querySelector('.right .top img').src=src;
+}
+const text=document.querySelector(".secon");
+const tyoing=()=>{
+    setTimeout(()=>{
+        text.innerHTML="for graphic design"
+    },0)
+    setTimeout(()=>{
+        text.innerHTML="and"
+    },4000)
+    setTimeout(()=>{
+        text.innerHTML="digital markating"
+    },8000)
+}
+tyoing()
+setInterval(tyoing,12000)
